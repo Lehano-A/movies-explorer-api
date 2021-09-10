25c23ffda5d19e7004ea974e9b52928e1e532391
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2021-09-10 23:50:33 +0300

    Created 'User' scheme

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const { isEmail } = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: (v) => isEmail(v),
+  },
+
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+
+  name: {
+    type: String,
+    default: 'Например: Александр или Мария',
+    required: true,
+    minLength: 2,
+    maxLength: 30,
+  },
+}, { versionKey: false });
+
+module.exports = mongoose.model('user', userSchema);
